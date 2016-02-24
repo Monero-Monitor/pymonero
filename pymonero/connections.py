e402b23bcf40662aef4bb5cbb4c5f1850830ec92
Mike C <micro-machine@users.noreply.github.com>
2016-02-24 18:54:00 -0500

    add Wallet connection class

--- pymonero/connections.py
+++ pymonero/connections.py
@@ -11,3 +11,15 @@ class Daemon:
         
     def to_JSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=2)
+
+class Wallet:
+    def __init__(self, rpchost=None, rpcport=None):
+        self.HOST = rpchost if rpchost is not None else 'http://localhost'
+        self.PORT = rpcport if rpcport is not None else '18082'
+        self.URL                 = self.HOST + ':' + self.PORT
+        self.RPC_URL             = self.URL + '/json_rpc'
+        self.RPC_STANDARD_HEADER = {'content-type': 'application/json'}
+        self.RPC_STANDARD_VALUES = {"jsonrpc": "2.0", "id": "0"}
+        
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=2)
