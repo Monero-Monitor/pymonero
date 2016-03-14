adf31761306480a55621ef89082c10621db73c7f
Mike C <micro-machine@users.noreply.github.com>
2016-03-13 22:27:08 -0600

    reorg and add a lot of error handling to bitmonerod rpc
    bitmonerod updates:
    
    - new function daemonJSONrpc greatly reduces code repetition.
    - error returns introduced to improve usability.

--- pymonero/bitmonerod/rpc.py
+++ pymonero/bitmonerod/rpc.py
@@ -1,175 +1,221 @@
 import requests as req
 import json
 from . import classes
+from .. import utils
+
+def daemonJSONrpc(daemon, rpc_input):
+    '''daemonJSONrpc() :: Send daemon JSON_RPC method and process initial result.'''
+    
+    # Add standard rpc values
+    rpc_input.update(daemon.RPC_STANDARD_VALUES)
+    
+    # Execute the rpc request and return response output
+    try:
+        resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+        output = resp.json()
+        
+        # Return result or error message from rpc call
+        if "result" in output:
+            result = output["result"]
+            return result, 0
+        else:
+            error = utils.ErrorMessage(output["error"]["message"])
+            code = output["error"]["code"]
+            if code == 0:
+                code = -1
+            return error, code
+    except:
+        result = utils.ErrorMessage("Error returning result from 'daemonJSONrpc'.")
+        return result, 1
+
+def daemonRPC(daemon, interface, rpc_input):
+    '''daemonRPC() :: Send daemon RPC method and process initial result.'''
+    
+    method_url = daemon.URL + interface
+    
+    # Execute the rpc request and return response output
+    try:
+        resp = req.post(method_url,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+        output = resp.json()
+        
+        return output, 0
+    except:
+        error = utils.ErrorMessage("Error returning result from 'daemonRPC'.")
+        return error, 1
 
 def getDaemonInfo(daemon):
     ''' getDaemonInfo() :: Function that returns "get_info" rpc call info. '''
     
     # Create rpc data input
-    rpc_input = { "method": "get_info" }         # bitmonerod rpc method
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
+    rpc_input = { "method": "get_info" } # Get RPC result
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
-    
-    if not "result" in output:
-        errormsg = "getDaemonInfo: 'result' not found in reply."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-        return
-    
-    # Organize rpc response output and return
-    info = classes.DaemonInfo(output["result"])
-    return info
+    # Return formatted result
+    if err == 0:
+        try:
+            info = classes.DaemonInfo(result)
+            return info, 0
+        except:
+            error = utils.ErrorMessage("Error returning result from 'getDaemonInfo'.")
+            return error, 1
+    else:
+        return result, err
 
 def getConnections(daemon):
-    ''' getConnections() :: Function that returns "get_connections" rpc call info. '''
+    '''getConnections() :: Function that returns "get_connections" rpc call info.'''
     
     # Create rpc data input
-    rpc_input = { "method": "get_connections" }  # bitmonerod rpc method
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
-
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
+    rpc_input = { "method": "get_connections" } # Get RPC result
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    if not "result" in output:
-        errormsg = "getConnections: 'result' not found in reply."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-        return
-    
-    # Organize rpc response output and return
-    connections = classes.Connections(output["result"])
-    return connections
+    # Return formatted result
+    if err == 0:
+        try:
+            connections = classes.Connections(result)
+            return connections, 0
+        except:
+            error = utils.ErrorMessage("Error returning result from 'getConnections'.")
+            return error, 1
+    else:
+        return result, err
 
 def hardForkInfo(daemon):
     ''' hardForkInfo() :: Function that returns "hard_fork_info" rpc call info. '''
     
     # Create rpc data input
-    rpc_input = { "method": "hard_fork_info" }  # bitmonerod rpc method
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
-
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
+    rpc_input = { "method": "hard_fork_info" } # Get RPC result
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    if not "result" in output:
-        errormsg = "getBlockCount: 'result' not found in reply."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-        return
-        
-    hard_fork_info = classes.HardForkInfo(output["result"])
-    return hard_fork_info
+    # Return formatted result
+    if err == 0:
+        try:
+            hard_fork_info = classes.HardForkInfo(result)
+            return hard_fork_info, 0
+        except:
+            error = utils.ErrorMessage("Error returning result from 'hardForkInfo'.")
+            return error, 1
+    else:
+        return result, err
 
 def getBlockCount(daemon):
     ''' getBlockCount() :: Function that returns "getblockcount" rpc call info. '''
     
     # Create rpc data input
-    rpc_input = { "method": "getblockcount" }  # bitmonerod rpc method
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
-
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
+    rpc_input = { "method": "getblockcount" } # Get RPC result
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    if not "result" in output:
-        errormsg = "getBlockCount: 'result' not found in reply."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-        return
-    
-    if output["result"]["status"] != "OK":
-        errormsg = "getBlockCount: Status not OK"
-        print(errormsg)
-        return 0
+    # Return formatted result
+    if err == 0:
+        try:
+            count = int(result["count"])
+            return count, 0
+        except:
+            error = utils.ErrorMessage("Error returning result from 'getBlockCount'.")
+            return error, 1
     else:
-        count = output["result"]["count"]
-        return count
+        return result, err
 
 def getTransactions(daemon,hashes,block=None):
     ''' getDaemonInfo(hashes,[block]) :: Function that returns "get_info" rpc call info. '''
     
-    # bitmonerod url
-    url = daemon.URL + "/gettransactions"
+    # RPC method
+    method = "/gettransactions"
     
+    # Create bitmonerod rpc method input
     if isinstance(hashes, str):
         txs_hashes = [ hashes ]
     else:
         txs_hashes = hashes
-    # Create bitmonerod rpc method input
     rpc_input = { "txs_hashes": txs_hashes, "decode_as_json": True }
-
-    # Execute the rpc request and store response output
-    resp = req.post(url,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
     
-    if not "txs_as_json" in output:
-        errormsg = "getTransactions: 'txs_as_json' not found in reply."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-        return
+    output, err = daemonRPC(daemon, method, rpc_input)
     
-    # Organize rpc response output and return
-    txs_as_json = output["txs_as_json"]
-    n_txs = len(txs_as_json)
-    transactions = []
-    for i in range(0,n_txs):
-        output_json_str = str(txs_as_json[i])
-        output_json_str.replace('\\', '')
-        output_json = json.loads(output_json_str)
-        transactions.append(classes.Transaction(hashes[i], output_json, block))
-    return transactions
+    if err == 0:
+        if "missed_tx" in output:
+            error = utils.ErrorMessage("Error returning result from 'getTransactions': missed_tx")
+            error.missed_tx = output["missed_tx"]
+            return error, -1
+        else:
+            try:
+                txs_as_json = output["txs_as_json"]
+                n_txs = len(txs_as_json)
+                transactions = []
+                for i in range(0,n_txs):
+                    output_json_str = str(txs_as_json[i])
+                    output_json_str.replace('\\', '')
+                    output_json = json.loads(output_json_str)
+                    transactions.append(classes.Transaction(hashes[i], output_json, block))
+                return transactions, 0
+            except:
+                print(json.dumps(output, indent=2))
+                error = utils.ErrorMessage("Error returning result from 'getTransactions'.")
+                return error, 1
+    else:
+        return output, err
 
 def _getBlockAndParse(daemon,rpc_input):
     ''' _getBlockAndParse(rpc_input) :: Function called by getBlockByHash &
-        getBlockByHeight to return block info. '''
+        getBlockByHeight to return block info. There is some weird stuff going on with
+        block information that prevents being about to use the standard daemonJSONrpc()
+        function. '''
     
     # Add standard rpc values to input
     rpc_input.update(daemon.RPC_STANDARD_VALUES)
     
-    # Execute the rpc request
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+    try:
+        # Execute the rpc request
+        resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+        
+        # Rpc response output
+        output = json.loads( "\n".join(filter(lambda l: "blob" not in l, resp.text.split("\n"))))
+        
+        if "result" in output:
+            result = output["result"]
+        else:
+            error = utils.ErrorMessage(output["error"]["message"])
+            code = output["error"]["code"]
+            if code == 0:
+                code = -1
+            return error, code
+        
+    except:
+        error = utils.ErrorMessage("Error returning result from '_getBlockAndParse'.")
+        return error, 1
     
-    # Rpc response output
-    output = json.loads( "\n".join(filter(lambda l: "blob" not in l, resp.text.split("\n"))))
-    
-    if not "result" in output:
-        return 1
-    
-    # Gather block header:
-    block_header = output["result"]["block_header"];
-    
-    # Gather block details:
-    output_json_str = str(output["result"]["json"])
-    output_json_str.replace('\\', '')
-    output_json = json.loads(output_json_str)
-    
-    # Get block info
-    blockInfo = classes.BlockInfo(block_header, output_json)
-    
-    # Return daemon info
-    return blockInfo
+    try:
+        # Gather block header:
+        block_header = result["block_header"]
+        
+        # Gather block details:
+        output_json_str = str(output["result"]["json"])
+        output_json_str.replace('\\', '')
+        output_json = json.loads(output_json_str)
+        
+        # Get block info
+        blockInfo = classes.BlockInfo(block_header, output_json)
+        
+        # Return daemon info
+        return blockInfo, 0
+    except:
+        error = utils.ErrorMessage("Error returning block info from '_getBlockAndParse'.")
+        return error, 1
 
 def getBlockByHeight(daemon,block_height):
     ''' getBlockByHeight(block_height) :: Function that returns "getblock" rpc call info
         with "height" param. '''
     
-    # Define bitmonerod rpc method input
-    rpc_input = { "method": "getblock", "params": { "height": block_height } }
+    if isinstance(block_height, int):
+        # Define bitmonerod rpc method input
+        rpc_input = { "method": "getblock", "params": { "height": block_height } }
     
-    # Get block and parse into structure
-    blockInfo = _getBlockAndParse(daemon,rpc_input)
-    
-    if blockInfo == 1:
-        errormsg = "getBlockByHeight: 'result' not found in reply (in '_getBlockAndParse')."
-        print(errormsg)
-        raise RuntimeError(errormsg)
-    
-    # Return daemon info
-    return blockInfo
+        # Get block and parse into structure
+        blockInfo, err = _getBlockAndParse(daemon,rpc_input)
+        
+        # Return daemon info
+        return blockInfo, err
+    else:
+        error = utils.ErrorMessage('Block height' + str(block_height) + 'is invalid: not an integer.')
+        return error, 1
 
 def getBlockByHash(daemon,block_hash):
     ''' getBlockByHash(block_hash) :: Function that returns "getblock" rpc call info with
@@ -179,15 +225,10 @@ def getBlockByHash(daemon,block_hash):
     rpc_input = { "method": "getblock", "params": { "hash": block_hash } }
     
     # Get block and parse into structure
-    blockInfo = _getBlockAndParse(daemon,rpc_input)
-    
-    if blockInfo == 1:
-        errormsg = "getBlockByHash: 'result' not found in reply (in '_getBlockAndParse')."
-        print(errormsg)
-        raise RuntimeError(errormsg)
+    blockInfo, err = _getBlockAndParse(daemon,rpc_input)
     
     # Return daemon info
-    return blockInfo
+    return blockInfo, err
 
 def _getBlockHeaderAndParse(daemon,rpc_input):
     ''' _getBlockAndParse(rpc_input) :: Function called by getBlockByHash &
@@ -196,20 +237,38 @@ def _getBlockHeaderAndParse(daemon,rpc_input):
     # Add standard rpc values to input
     rpc_input.update(daemon.RPC_STANDARD_VALUES)
     
-    # Execute the rpc request
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+    try:
+        # Execute the rpc request
+        resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
+        
+        # Rpc response output
+        output = json.loads( "\n".join(filter(lambda l: "blob" not in l, resp.text.split("\n"))))
+        
+        if "result" in output:
+            result = output["result"]
+        else:
+            error = utils.ErrorMessage(output["error"]["message"])
+            code = output["error"]["code"]
+            if code == 0:
+                code = -1
+            return error, code
+        
+    except:
+        error = utils.ErrorMessage("Error returning result from '_getBlockHeaderAndParse'.")
+        return error, 1
     
-    # Rpc response output
-    output = json.loads( "\n".join(filter(lambda l: "blob" not in l, resp.text.split("\n"))))
-    
-    # Gather block header:
-    block_header = output["result"]["block_header"];
-    
-    # Get block info
-    blockHeader = classes.BlockHeader(block_header)
-    
-    # Return daemon info
-    return blockHeader
+    try:
+        # Gather block header:
+        block_header = result["block_header"];
+        
+        # Get block info
+        blockHeader = classes.BlockHeader(block_header)
+        
+        # Return daemon info
+        return blockHeader, 0
+    except:
+        error = utils.ErrorMessage("Error returning header info from '_getBlockHeaderAndParse'.")
+        return error, 1
 
 def getBlockHeaderByHeight(daemon,block_height):
     ''' getBlockHeaderByHeight(block_height) :: Function that returns
@@ -220,25 +279,30 @@ def getBlockHeaderByHeight(daemon,block_height):
         rpc_input = { "method": "getblockheaderbyheight", "params": { "height": block_height } }
         
         # Get block and parse into structure
-        blockHeader = _getBlockHeaderAndParse(daemon,rpc_input)
+        blockHeader, err = _getBlockHeaderAndParse(daemon,rpc_input)
     
         # Return daemon info
-        return blockHeader
+        return blockHeader, err
     else:
-        print('Block height' + str(block_height) + 'is invalid: not an integer.')
+        error = utils.ErrorMessage('Block height' + str(block_height) + 'is invalid: not an integer.')
+        return error, 1
 
 def getBlockHeaderByHash(daemon,block_hash):
     ''' getBlockHeaderByHash(block_hash) :: Function that returns "getblockheaderbyheight"
         rpc call info with "hash" param. '''
     
-    # Define bitmonerod rpc method input
-    rpc_input = { "method": "getblockheaderbyhash", "params": { "hash": block_hash } }
+    if isinstance(block_hash, str):
+        # Define bitmonerod rpc method input
+        rpc_input = { "method": "getblockheaderbyhash", "params": { "hash": block_hash } }
+        
+        # Get block and parse into structure
+        blockHeader, err = _getBlockHeaderAndParse(daemon,rpc_input)
     
-    # Get block and parse into structure
-    blockHeader = _getBlockHeaderAndParse(daemon,rpc_input)
-    
-    # Return daemon info
-    return blockHeader
+        # Return daemon info
+        return blockHeader, err
+    else:
+        error = utils.ErrorMessage('Block hash' + str(block_hash) + 'is invalid: not a string.')
+        return error, 1
 
 def getLastBlockHeader(daemon):
     ''' getLastBlockHeader() :: Function that returns "getlastblockheader" rpc call info. '''
@@ -247,25 +311,21 @@ def getLastBlockHeader(daemon):
     rpc_input = { "method": "getlastblockheader" }
     
     # Get block and parse into structure
-    blockHeader = _getBlockHeaderAndParse(daemon,rpc_input)
+    blockHeader, err = _getBlockHeaderAndParse(daemon,rpc_input)
     
     # Return daemon info
-    return blockHeader
+    return blockHeader, err
 
 def getBlockHash(daemon, block_height):
     ''' getBlockHash(block_height) :: Function that returns "on_getblockhash" rpc call info. '''
     
     # Define bitmonerod rpc method input
     rpc_input = { "method": "on_getblockhash", "params": [ block_height ] }
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
+    # Return formatted result (valid result is just a string of the hash).
+    return result, err
     
-    #hash = { "hash": output["result"], "height": block_height }
-    hash = output["result"]
-    return hash
 
 def getBlockTemplate(daemon, wallet_address, reserve_size):
     ''' getDaemonInfo(wallet_address, reserve_size) :: Function that returns
@@ -275,12 +335,15 @@ def getBlockTemplate(daemon, wallet_address, reserve_size):
     rpc_input = { "method": "getblocktemplate", 
                     "params": { "wallet_address": wallet_address, "reserve_size": reserve_size }
                 } # bitmonerod rpc method
-    rpc_input.update(daemon.RPC_STANDARD_VALUES) # add standard rpc values
+    result, err = daemonJSONrpc(daemon, rpc_input)
     
-    # Execute the rpc request and store response output
-    resp = req.post(daemon.RPC_URL,data=json.dumps(rpc_input),headers=daemon.RPC_STANDARD_HEADER)
-    output = resp.json()
-    
-    # Organize rpc response output and return
-    info = classes.BlockTemplate(output["result"])
-    return info
+    # Return formatted result
+    if err == 0:
+        try:
+            info = classes.BlockTemplate(result)
+            return info, 0
+        except:
+            error = utils.ErrorMessage("Error returning result from 'getBlockTemplate'.")
+            return error, 1
+    else:
+        return result, err
